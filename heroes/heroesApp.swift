91f4f8d6416b0e828f68da87e80bee7a9eef2296
Матвей Андреевич Ганин <ganin@ao-nk.ru>
2022-09-20 12:04:06 +0300

    Initial Commit

---
+++ heroes/heroesApp.swift
@@ -0,0 +1,17 @@
+//
+//  heroesApp.swift
+//  heroes
+//
+//  Created by matvey on 20.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct heroesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
